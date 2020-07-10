e4f192e9ff4c4b77ad02350b0796458672fc000f
Dmitriy Davydov <Dmitriy.Davydov.ua@gmail.com>
2020-07-10 10:14:52 +0300

    chore(test) add simple test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Folder tree name', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Folder tree/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
 });
